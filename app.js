599e9f43c63edb628cceb552ce9597855a52e70f
Druhin Tarafder <contact@druh.in>
2019-11-09 20:29:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,53 +1,65 @@
-		//bg
-		TweenMax.to(".bg", 2, {
-			width: "100vw",
-			ease: Expo.easeInOut
-		})
+//bg
+TweenMax.to(".bg", 2, {
+	width: "100vw",
+	ease: Expo.easeInOut
+})
 
-		// navbar - (logo - ham menu)
-		TweenMax.from(".navbar", 1.5, {
-			delay: 3.5,
-			opacity: 0,
-			y: 25,
-			ease: Expo.easeInOut
-		})
+// navbar - (logo - ham menu)
+TweenMax.from(".navbar", 1.5, {
+	delay: 3.5,
+	opacity: 0,
+	y: 25,
+	ease: Expo.easeInOut
+})
 
-		// first name
-		TweenMax.from("section div h1", 1.5, {
-			delay: 1.6,
-			opacity: 0,
-			y: 50,
-			ease: Expo.easeInOut
-		})
+// first name
+TweenMax.from("section div h1", 1.5, {
+	delay: 1.6,
+	opacity: 0,
+	y: 50,
+	ease: Expo.easeInOut
+})
 
-                // last name
-		TweenMax.from("section div h2", 1.5, {
-			delay: 2,
-			opacity: 0,
-			y: 50,
-			ease: Expo.easeInOut
-		})
-                
-                // description
-		TweenMax.from("section h3", 1.5, {
-			delay: 3,
-			opacity: 0,
-			y: 50,
-			ease: Expo.snap
-		})
+// last name
+TweenMax.from("section div h2", 1.5, {
+	delay: 2,
+	opacity: 0,
+	y: 50,
+	ease: Expo.easeInOut
+})
 
-		//works-text
-		TweenMax.from(".works h1", 1.5, {
-			delay: 3,
-			opacity: 0,
-			y: 50,
-			ease: Expo.snap
-		})
+// description
+TweenMax.from("section h3", 1.5, {
+	delay: 3,
+	opacity: 0,
+	y: 50,
+	ease: Expo.snap
+})
 
-		// resume-box
-		TweenMax.from(".resume", 1.5, {
-			delay: 3.5,
-			opacity: 0,
-			y: 25,
-			ease: Expo.easeInOut
-		})
+//works-text
+TweenMax.from(".works h1", 1.5, {
+	delay: 3,
+	opacity: 0,
+	y: 50,
+	ease: Expo.snap
+})
+
+// resume-box
+TweenMax.from(".resume", 1.5, {
+	delay: 3.5,
+	opacity: 0,
+	y: 25,
+	ease: Expo.easeInOut
+})
+
+var image = document.images[2]
+var bigimage = document.createElement("img")
+
+bigimage.onload = function () {
+	image.src = this.src
+}
+
+
+setTimeout(function () {
+	bigimage.src = "./assets/mainimg.png";
+}, 50)
