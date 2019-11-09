0955c302b6e54a15ea65a7a70325fdb253bed0b8
Druhin Tarafder <contact@druh.in>
2019-11-09 18:37:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
+		//bg
+		TweenMax.to(".bg", 2, {
+			width: "100vw",
+			ease: Expo.easeInOut
+		})
 
+		// navbar
+		TweenMax.from(".navbar", 1.5, {
+			delay: 3.5,
+			opacity: 0,
+			y: 25,
+			ease: Expo.easeInOut
+		})
+
+		// TEXT
+		TweenMax.from("section div h1", 1.5, {
+			delay: 1.6,
+			opacity: 0,
+			y: 50,
+			ease: Expo.easeInOut
+		})
+
+		TweenMax.from("section div h2", 1.5, {
+			delay: 2,
+			opacity: 0,
+			y: 50,
+			ease: Expo.easeInOut
+		})
+
+		TweenMax.from("section h3", 1.5, {
+			delay: 3,
+			opacity: 0,
+			y: 50,
+			ease: Expo.snap
+		})
